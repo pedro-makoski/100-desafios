9a7939e3c5aa6f3836b053216387287c7edf700f
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2025-03-01 08:50:10 -0300

    Melhorias desafio 037

--- Desafio-036-publicar-blog-obsidian-plugin/rantool-md-to-html/module.js
+++ Desafio-036-publicar-blog-obsidian-plugin/rantool-md-to-html/module.js
@@ -7,18 +7,14 @@ class Texts {
         const lines = [];
 
         let actual = [];
-        let array = Array.from(this.text)
-        array.push(" ")
-        array.map((caracter, index) => {
-            if(caracter !== end) {
-                actual.push(caracter)
-            }
-
-            if(caracter === end || index === array.length-1) {
+        Array.from(this.text).map(caracter => {
+            if(caracter === end) {
                 const text = actual.join("");
                 actual = []; 
                 lines.push(text);
             }
+
+            actual.push(caracter)
         });
 
         return {
@@ -70,7 +66,6 @@ class MD {
         this.linesMod = this.texts.listOfLines("\n");
         this.allLines = this.linesMod.removeItemsInLines(["\n", "\r"]);
         this.identifierLines = [];
-        this.identifierLinesIndicatedLists = {};
         this.posOfPartOfTableToExclude = [];
         this.openIdentControlLists = {}
         this.openIdentControlListsWithTagCloseValue = {}
@@ -81,8 +76,7 @@ class MD {
 
         if(line === EMPTY_STRING) {
             this.identifierLines[index] = WRAP_NAME;
-            // this.posOfPartOfTableToExclude.push(index)
-            return `<br>`
+            this.posOfPartOfTableToExclude.push(index)
         }
 
         return line; 
@@ -94,7 +88,7 @@ class MD {
         if(hashtags) {
             const hashtagsList = hashtags[1].split("");
             const text = hashtags[2];
-            this.identifierLines[index] = `${HEADER_NAEME}-${hashtagsList.length}`;
+            this.identifierLines[index] = HEADER_NAEME;
             return `<h${hashtagsList.length}>${text}</h${hashtagsList.length}>`;
         }
 
@@ -216,7 +210,6 @@ class MD {
         const match = line.match(pattern);
         if(match) {
             this.identifierLines[index] = id;
-            this.identifierLinesIndicatedLists[index] = id;
             const isFirst = index === 0 || !this.openIdentControlLists[match[1].length] || this.identifierLines[index-1]!==id;
             let newLine =  `${isFirst ? getOpenListTag(match[2]) : ""}${openPosElement}${match[indexOfContent]}${closePoselement}`;
             if(isFirst) {
@@ -261,16 +254,13 @@ class MD {
     doNoOrdenerdList(line, index, openListTag, closeListTag, openPosElement, closePoselement) {
         const pattern = /^(\s*)-\s([\s\S]*)/
         const patternOl = /^(\s*)\d+\.\s([\s\S]*)/
-        return this.doListsWithTab(pattern, line, index, () => `<ul>`, closeListTag, openPosElement, closePoselement, LIST_NO_ORDER_NAME, 2, patternOl, "</ol>", false)
+        return this.doListsWithTab(pattern, line, index, () => openListTag, closeListTag, openPosElement, closePoselement, LIST_NO_ORDER_NAME, 2, patternOl, "</ol>")
     }
 
     doOrderedLists(line, index, openListTag, closeListTag, openPosElement, closePoselement) {
         const pattern = /^(\s*)(\d+)\.\s([\s\S]*)/
         const patternUl = /^(\s*)-\s([\s\S]*)/
-        return this.doListsWithTab(pattern, line, index, (content) => {
-            this.identifierLinesIndicatedLists[index] = content;
-            return `<ol start="${content}">`
-        }, closeListTag, openPosElement, closePoselement, LIST_ORDER_NAME, 3, patternUl, "</ul>", true)
+        return this.doListsWithTab(pattern, line, index, (content) => `<ol start="${content}">`, closeListTag, openPosElement, closePoselement, LIST_ORDER_NAME, 3, patternUl, "</ul>")
     }
 
     doBlockQuote(line, index, openBlockQuoteTag, closeBlockQuoteTag) {
@@ -426,6 +416,7 @@ class MD {
     }
 
     convertTOHTML() {
+        console.log(this.allLines)
         this.allLines = this.doFuncionsAboutLine([
             (l, i) => this.emptyStringIsBr(l, i),
             (l, i) => this.putBold(l, i, "<strong>", "</strong>"),
@@ -454,3 +445,7 @@ class MD {
         return result; 
     }
 }
+
+
+
+module.exports = MD;
